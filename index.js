1d51077042f5f9861244891c9d806aacc5bd70f7
ohexoh <179223985+ohexoh@users.noreply.github.com>
2024-09-06 18:52:33 +1000

    changed every instance of "guest" to "away"
    "client" wanted the scoreboard to say "away" instead of "guest"

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let homePoint1 = document.getElementById("home-score-btn-1");
 let homeScoreEl = document.getElementById("home-score");
 let homeScore = 0;
-let guestPoint1 = document.getElementById("guest-score-btn-1");
-let guestScoreEl = document.getElementById("guest-score");
-let guestScore = 0;
+let awayPoint1 = document.getElementById("away-score-btn-1");
+let awayScoreEl = document.getElementById("away-score");
+let awayScore = 0;
 
 function homePointButton1() {
   homeScore += 1;
@@ -19,16 +19,16 @@ function homePointButton3() {
   homeScoreEl.textContent = homeScore;
 }
 
-function guestPointButton1() {
-  guestScore += 1;
-  guestScoreEl.textContent = guestScore;
+function awayPointButton1() {
+  awayScore += 1;
+  awayScoreEl.textContent = awayScore;
 }
 
-function guestPointButton2() {
-  guestScore += 2;
-  guestScoreEl.textContent = guestScore;
+function awayPointButton2() {
+  awayScore += 2;
+  awayScoreEl.textContent = awayScore;
 }
-function guestPointButton3() {
-  guestScore += 3;
-  guestScoreEl.textContent = guestScore;
+function awayPointButton3() {
+  awayScore += 3;
+  awayScoreEl.textContent = awayScore;
 }
